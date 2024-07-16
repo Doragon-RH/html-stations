99103c867d41610fcfb5382f0b783f28ee63c0c9
Doragon-RH <r216af@icloud.com>
2024-07-16 15:23:29 +0900

    station15 clear!

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,16 +1,32 @@
 async function getData() {
-  // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
-  // ３秒後にフルネームのリストを表示されるようにしましょう。
-  // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return result;
 }
 
-function test() {
-  return
+function test(data) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // データから氏名を抽出
+      const names = data.map((user) => ({
+        full_name: user.family_name + " " + user.first_name,
+      }));
+      resolve(names);
+    }, 3000);
+  });
 }
-
